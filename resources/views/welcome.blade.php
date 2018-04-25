49c18c25ba1260e9874838e40e5ee767424aa7aa
Ronen <ronenamon88@gmail.com>
2018-04-25 21:52:00 +0300

    change name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Ronen Project
                 </div>
 
                 <div class="links">
