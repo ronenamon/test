446de97d697d944a39ff35c4dc89d135ff808e5b
Ronen <ronenamon88@gmail.com>
2018-04-27 21:23:39 +0300

    add morgan , add mysql connections

--- public/server/index.js
+++ public/server/index.js
@@ -17,35 +17,84 @@ app.get('/', (req, res) => {
     res.send('hello this is default route API <br> ' +
         'Use this : ' +
         '<ul>' +
-        '<li>/photo/{id}</li>'+
-         '<li>/all</li>' +
+        '<li>/user/{id}</li>'+
+         '<li>/users</li>' +
          '<ul>'
     );
-
-
-
 });
 
 //get list of users
 app.get('/users' , (req,res)=> {
     console.log("users route request...");
 
-    res.json({
-        anObject: { item1: "item1val", item2: "item2val" },
-        anArray: ["item1", "item2"],
-        another: "item"
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456789',
+        database: 'ronen'
+    });
+
+
+    const queryString = "SELECT * FROM user_tbl";
+
+
+    connection.query(queryString ,(err , rows, fields)=>{
+        if (err){
+            console.log("Faild to query for users " + err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log("user fetched successfully");
+        //map
+
+        const users = rows.map((row)=>{
+            return {
+                firstName : row.first_name,
+                lastName  : row.last_name
+            }
+        });
+        res.json(users);
     });
 
 });
 
 
 //get user by id
-app.get('/user/:id' , (req,res)=>{
+app.get('/user/:id' , (req,res)=> {
     console.log("user route request with id" + req.params.id);
 
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456789',
+        database: 'ronen'
+    });
 
 
-    res.end();
+    const userId = req.params.id;
+    const queryString = "SELECT * FROM user_tbl WHERE id = ?";
+
+
+    connection.query(queryString ,[userId] ,(err , rows, fields)=>{
+        if (err){
+            console.log("Faild to query for users " + err);
+            res.sendStatus(500);
+            return;
+        }
+        console.log("user fetched successfully");
+        //map
+
+        const users = rows.map((row)=>{
+           return {
+               firstName : row.first_name,
+               lastName  : row.last_name
+               }
+        });
+        res.json(users);
+    });
+
+
+    //res.end();
 });
 
 
