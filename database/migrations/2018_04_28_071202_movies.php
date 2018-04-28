e4a6787bc1c7202f7f831bb412ba61bfb5f9501f
Ronen <ronenamon88@gmail.com>
2018-04-28 10:20:38 +0300

    add query

--- database/migrations/2018_04_28_071202_movies.php
+++ database/migrations/2018_04_28_071202_movies.php
@@ -14,6 +14,12 @@ class Movies extends Migration
     public function up()
     {
         //
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('link');
+            $table->timestamps();
+        });
     }
 
     /**
