e47c743f6ee37c1a5f441a031859a64ae76996c2
Ronen <ronenamon88@gmail.com>
2018-04-28 10:28:44 +0300

    fix

--- database/migrations/2018_04_28_072448_create_movies_table.php
+++ database/migrations/2018_04_28_072448_create_movies_table.php
@@ -14,6 +14,12 @@ class CreateMoviesTable extends Migration
     public function up()
     {
         //
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('link');
+            $table->timestamps();
+        });
     }
 
     /**
